47748d5dfc3839a76992941818d0c661b7e5f751
ittaico <ittai.corem@gmail.com>
2019-12-03 05:19:50 +0200

    starting question 1.1

---
+++ Chapter 1.js
@@ -0,0 +1,12 @@
+/**
+ * Question 1.1
+ * @param str - The input string.
+ * @returns True if all the Characters ate unique, otherwise false.
+ */
+function isUnique(str){
+    var asciiSize = 256;
+    var Array = [256];
+    console.log(Array.length);
+}
+
+isUnique('avvv');
