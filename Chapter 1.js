53ec7213dba269c9bba51c5fb59701e96665f130
ittaico <ittai.corem@gmail.com>
2019-12-03 05:30:20 +0200

    starting 1.1

--- Chapter 1.js
+++ Chapter 1.js
@@ -4,9 +4,7 @@
  * @returns True if all the Characters ate unique, otherwise false.
  */
 function isUnique(str){
-    var asciiSize = 256;
-    var Array = [256];
-    console.log(Array.length);
+   
 }
 
 isUnique('avvv');
